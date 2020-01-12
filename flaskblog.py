d066eb1f772ae003d03da899e177aa338e00b61c
Mason Bose <masonbose1231@gmail.com>
2020-01-12 12:52:54 -0500

    Some more design/architecture changes

--- flaskblog.py
+++ flaskblog.py
@@ -1,5 +1,8 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '67d345fd46e319b922b5073b610356ea'
 
 posts = [
     {
@@ -7,7 +10,7 @@ posts = [
         'title': 'Blog Post 1',
         'content': 'First post content.',
         'date_posted': 'April 7, 2020'
-    }, 
+    },
     {
         'author': 'Jane Doe',
         'title': 'Blog Post 2',
@@ -16,14 +19,32 @@ posts = [
     }
 ]
 
+
 @app.route("/")
 @app.route("/home")
 def home():
     return render_template('home.html', posts=posts)
 
+
 @app.route("/about")
 def about():
     return render_template('about.html', title='About')
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))  # redirect to home function above
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Login', form=form)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
